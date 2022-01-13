151fcfd3b46f16387baee2beb5182b87af836300
miracle-man <62384215+miracle-man@users.noreply.github.com>
2022-01-13 10:50:21 +0300

    verstka1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,15 @@ import Navigation from "../Navigation/Navigation";
 export default function Header({ mainStyleHeader, loggedIn, visibleHeader }) {
   return (
     <>
-      <header
+      {/* <header
         className={` ${
           visibleHeader ? "header" : "display-none"
         } ${mainStyleHeader ? "" : "header_style_movie"} `}
+      >
+        <Navigation loggedIn={loggedIn} place="header" />
+      </header> */}
+      <header
+        className="header"
       >
         <Navigation loggedIn={loggedIn} place="header" />
       </header>
