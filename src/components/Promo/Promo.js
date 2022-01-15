39b38fcfeb4da8e019b28f8dd3a0c01bd20e75d8
miracle-man <62384215+miracle-man@users.noreply.github.com>
2022-01-16 00:08:05 +0300

    rev1-2

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,16 +4,16 @@ import "./promo.css";
 export default function Promo() {
   return (
     <section className="promo">
-    <div className="promo__textBlock">
-      <h1 className="promo__title">
-      Учебный проект студента факультета Веб-разработки.
-      </h1>
-      <p className="promo__subtitle">
-        Листайте ниже, чтобы узнать больше про этот проект и его создателя.    
-      </p>
-      <button className="promo__button">Узнать больше</button>
-    </div>
       <div className="promo__logo" />
+      <div className="promo__textBlock">
+        <h1 className="promo__title">
+          Учебный проект студента факультета Веб-разработки.
+        </h1>
+        <p className="promo__subtitle">
+          Листайте ниже, чтобы узнать больше про этот проект и его создателя.    
+        </p>
+        <button className="promo__button">Узнать больше</button>
+      </div>
     </section>
   );
 }
