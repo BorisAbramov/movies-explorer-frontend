1a80e79e628553347e2e47c0aac74bcf55a914eb
miracle-man <62384215+miracle-man@users.noreply.github.com>
2022-01-16 15:59:08 +0300

    rev2-promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,19 +1,16 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 import "./promo.css";
 
 export default function Promo() {
   return (
-    <section className="promo">
-      <div className="promo__logo" />
-      <div className="promo__textBlock">
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб-разработки.
-        </h1>
-        <p className="promo__subtitle">
-          Листайте ниже, чтобы узнать больше про этот проект и его создателя.    
-        </p>
-        <button className="promo__button">Узнать больше</button>
-      </div>
-    </section>
+    <section className='promo'>
+    <div className='promo__pic'/>
+    <div className='promo__wrapper'>
+      <h1 className='promo__title'>Учебный проект студента факультета Веб-разработки.</h1>
+      <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+      <NavLink className='promo__link' to='/movies'>Узнать больше</NavLink>
+    </div>
+  </section>
   );
 }
